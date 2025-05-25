7435f07f81e06ccb2777186b8444c2aabee1ad04
SonJH7 <sjunh02@naver.com>
2025-05-26 00:00:34 +0900

    리스트 페이지의 데이터를 유지시키기

--- Pokemon/src/app/store.js
+++ Pokemon/src/app/store.js
@@ -1,7 +1,34 @@
 // src/app/store.js
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import { configureStore } from '@reduxjs/toolkit';
 import pokemonReducer from '../features/pokemonSlice';
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
 
+// Persist 설정
+const persistConfig = { key: 'root', storage, whitelist: ['pokemon'] };
+const persistedReducer = persistReducer(persistConfig, pokemonReducer);
+
+// Store 생성
 export const store = configureStore({
-  reducer: { pokemon: pokemonReducer }
+  reducer: {
+    pokemon: persistedReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // redux-persist 액션 무시
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 });
+
+// Persistor 생성
+export const persistor = persistStore(store);
--- Pokemon/src/main.jsx
+++ Pokemon/src/main.jsx
@@ -1,13 +1,18 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './app/store';
 import GlobalStyle from './styles/GlobalStyle';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <Provider store={store}>
-   <GlobalStyle />
-   <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <GlobalStyle />
+      <App />
+    </PersistGate>
   </Provider>
 );
